b82e26bec94d0a491b8276c040c958d1da149e86
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2026-04-20 17:21:44 -0300

    se inicia la pagina de perfil

--- Pokedex/PokeApp/Perfil.aspx
+++ Pokedex/PokeApp/Perfil.aspx
@@ -1,8 +1,16 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Perfil.aspx.cs" Inherits="PokeApp.Perfil" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <div class="row">
-        <div class="col">
+        <div class="col-5">
             <h1>Pefil de usuario</h1>
+            <label class="form-label">Nombre</label>
+            <asp:TextBox runat="server" ID="txtNombre" CssClass="form-control"/>
+
+            <label class="form-label">Apellido</label>
+            <asp:TextBox runat="server" ID="txtApellido" CssClass="form-control"/>
+
         </div>
+
+        <div class="col-5"></div>
     </div>
 </asp:Content>
--- Pokedex/PokeApp/Perfil.aspx.designer.cs
+++ Pokedex/PokeApp/Perfil.aspx.designer.cs
@@ -13,5 +13,23 @@ namespace PokeApp
 
     public partial class Perfil
     {
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control txtApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtApellido;
     }
 }
