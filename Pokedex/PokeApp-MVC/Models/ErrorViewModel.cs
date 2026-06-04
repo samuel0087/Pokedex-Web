0e031de605d8059909bb379f989d05c5eaeaa642
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2026-06-04 00:18:27 -0300

    se creo proyecto mvc

---
+++ Pokedex/PokeApp-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PokeApp_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
